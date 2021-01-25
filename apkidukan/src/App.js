4194dec4a56aa34e26139aa04bb1ed8ba89aeeaf
avinash <avinash.ingole@exceloid.com>
2021-01-25 22:52:49 +0530

    added routing to menu item

--- apkidukan/src/App.js
+++ apkidukan/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import HomePage from './pages/homePage/homepage.component'
-
+import {Switch,Route} from 'react-router-dom'
+const HatsPage = ()=> (
+  <div>
+    <h2>This is HatsPage</h2>
+  </div>
+)
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact={true} path='/' component={HomePage} />
+        <Route exact={true} path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
